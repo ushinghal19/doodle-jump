ebbaade5453395ac320af4c0a6f755f75b46a86f
Utsav Shinghal <ushinghal19@gmail.com>
2020-12-06 10:00:05 +0800

    working moving doodler pixel left and right

--- doodlejump.s
+++ doodlejump.s
@@ -27,50 +27,90 @@
 .data
 displayAddress: .word 0x10008000
 displayMax: .word 0x10009000
-doodlerLocation: .word 0x10008fc0
+doodlerX: .word 0x0000003C
+doodlerY: .word 0x00000F00
 
 .text
 lw $s0, displayAddress 	# $s0 stores the base address for display
 lw $s1, displayMax	# $s1 stores the maximum display
-lw $s2, doodlerLocation # $s2 stores the doodler's location
-li $s3, 0xe6f7eb	# $s3 stores the background colour
-li $s4, 0x821012	# $s4 stores the platform colour
-li $s5, 0x0b2773	# $s5 stores the doodler colour
+lw $s2, doodlerX 	# $s2 stores the doodler's x
+lw $s3, doodlerY	# $s3 stores the doodler's y
+li $s4, 0xe6f7eb	# $s4 stores the background colour
+li $s5, 0x821012	# $s5 stores the platform colour
+li $s6, 0x0b2773	# $s6 stores the doodler colour
 
+li $s7, 0x0000000	# Game Over Condition
 # Main =====================================================
 main:
-jal DRAW_BACKGROUND
+jal DRAW_BACKGROUND	# Draws Background
+add $a2, $zero, $s2
+add $a3, $zero, $s3
 jal DRAW_DOODLER
 jal DRAW_PLATFORM
+jal GAME_LOOP
+
 j Exit
-# ==========================================================
+
+# Game Loop ================================================
+GAME_LOOP:
+beq $s7, 00000001, GAME_OVER
+
+lw $t1, 0xffff0000
+beq $t1, 1, KEYBOARD_INPUT
+
+RETURN_HERE:
+li $v0, 32
+li $a0, 100
+syscall
+add $a2, $zero, $s2
+add $a3, $zero, $s3
+jal DRAW_BACKGROUND
+jal DRAW_PLATFORM
+jal DRAW_DOODLER
+
+j GAME_LOOP
+
+GAME_OVER:
+jr $ra
+
+
+
 
 # Functions ================================================
+KEYBOARD_INPUT:
+lw $t2, 0xffff0004
+beq $t2, 97, MOVE_LEFT
+beq $t2, 100, MOVE_RIGHT
+
+MOVE_LEFT:
+add $t1, $zero, $s2
+addi $t1, $t1, -4
+add $s2, $zero, $t1
+j RETURN_HERE
+
+MOVE_RIGHT:
+add $t1, $zero, $s2
+addi $t1, $t1, +4
+add $s2, $zero, $t1
+j RETURN_HERE
 
 # Background
 DRAW_BACKGROUND:
 add $t9, $zero, $s0	# $t9 stores the value of the base address
-
 LOOP:			# For loop through each pixel
-sw $s3, 0($t9)		# Overwriting the colour at address $t9
+sw $s4, 0($t9)		# Overwriting the colour at address $t9
 beq $t9, $s1, END	# Checking if $t9 reached the max
-
 UPDATE:
 addi $t9, $t9, 4
 j LOOP
-
 END: 
 jr $ra
 
-# Doodler
+# Doodler (x,y)
 DRAW_DOODLER:
-add $t8, $zero, $s2
-sw $s5, -4($t8)
-sw $s5, 4($t8)
-sw $s5, -132($t8)
-sw $s5, -128($t8)
-sw $s5, -124($t8)
-sw $s5, -256($t8)
+add $t1, $s0, $a2
+add $t2, $t1, $a3
+sw $s6, 0($t2)
 jr $ra
 
 # Platforms
@@ -78,34 +118,14 @@ DRAW_PLATFORM:
 # Platform 1
 add $t7, $zero, $s0
 addi $t7, $t7, 260
-sw $s4, 0($t7)
-sw $s4, 4($t7)
-sw $s4, 8($t7)
-sw $s4, 12($t7)
-sw $s4, 16($t7)
-sw $s4, 20($t7)
-
-# Platform 2
-add $t7, $zero, $s0
-addi $t7, $t7, 720
-sw $s4, 0($t7)
-sw $s4, 4($t7)
-sw $s4, 8($t7)
-sw $s4, 12($t7)
-sw $s4, 16($t7)
-sw $s4, 20($t7)
-
-# Platform 3
-add $t7, $zero, $s0
-addi $t7, $t7, 1448
-sw $s4, 0($t7)
-sw $s4, 4($t7)
-sw $s4, 8($t7)
-sw $s4, 12($t7)
-sw $s4, 16($t7)
-sw $s4, 20($t7)
-
+sw $s5, 0($t7)
+sw $s5, 4($t7)
+sw $s5, 8($t7)
+sw $s5, 12($t7)
+sw $s5, 16($t7)
+sw $s5, 20($t7)
 jr $ra
+
 
 # =========================================================
 
