e0f1003a356abeff5124fd7c6e501b7e9728d8ea
Utsav Shinghal <ushinghal19@gmail.com>
2020-12-06 22:14:57 +0800

    working jumping up and down / moving left and right

--- doodlejump.s
+++ doodlejump.s
@@ -29,15 +29,17 @@ displayAddress: .word 0x10008000
 displayMax: .word 0x10009000
 doodlerX: .word 0x0000003C
 doodlerY: .word 0x00000F00
+backgroundColour: .word 0xe6f7eb
+doodlerColour: .word 0x0b2773
 
 .text
 lw $s0, displayAddress 	# $s0 stores the base address for display
 lw $s1, displayMax	# $s1 stores the maximum display
 lw $s2, doodlerX 	# $s2 stores the doodler's x
 lw $s3, doodlerY	# $s3 stores the doodler's y
-li $s4, 0xe6f7eb	# $s4 stores the background colour
-li $s5, 0x821012	# $s5 stores the platform colour
-li $s6, 0x0b2773	# $s6 stores the doodler colour
+li $s4, 0x00000000	# $s4 stores whether the doodler is moving up or down
+li $s5, 0x00000000	# $s5 stores the doodlers jump radius
+
 
 li $s7, 0x0000000	# Game Over Condition
 # Main =====================================================
@@ -57,6 +59,23 @@ j START_LOOP
 # Game Loop ================================================
 GAME_LOOP:
 beq $s7, 00000001, GAME_OVER
+
+li $v0, 32
+li $a0, 100
+syscall
+
+add $a2, $zero, $s2
+add $a3, $zero, $s3
+jal REMOVE_DOODLER
+
+beq $s4, 1, MOVE_DOWN
+beq $s4, 0, MOVE_UP
+
+AFTER_MOVING_UP_OR_DOWN:
+add $a2, $zero, $s2
+add $a3, $zero, $s3
+addi $s6, $s6, 1	
+jal DRAW_DOODLER
 
 li $v0, 32
 li $a0, 100
@@ -90,6 +109,30 @@ lw $t2, 0xffff0004
 beq $t2, 97, MOVE_LEFT
 beq $t2, 100, MOVE_RIGHT
 
+MOVE_UP:
+add $t1, $zero, $s3	# Adds the y coordinate to t1
+addi $t1, $t1, -128	# Subtracts one row from the y coordinate
+add $s3, $zero, $t1	# Saves this y coordinate back into s3
+addi $s5, $s5, 1	# Adds 1 to the jump radius
+beq $s5, 10, SWITCH_DOWN 	# Moves down if its jumped up 5
+j AFTER_MOVING_UP_OR_DOWN
+
+SWITCH_DOWN:
+li $s4, 1	# Makes moving down True
+j AFTER_MOVING_UP_OR_DOWN
+
+MOVE_DOWN:
+add $t1, $zero, $s3
+addi $t1, $t1, +128
+add $s3, $zero, $t1
+addi $s5, $s5, -1
+beq $s5, $zero, SWITCH_UP
+j AFTER_MOVING_UP_OR_DOWN
+
+SWITCH_UP:
+li $s4, 0	# Makes moving down False
+j AFTER_MOVING_UP_OR_DOWN
+
 MOVE_LEFT:
 add $t1, $zero, $s2
 addi $t1, $t1, -4
@@ -106,7 +149,8 @@ j RETURN_HERE
 DRAW_BACKGROUND:
 add $t9, $zero, $s0	# $t9 stores the value of the base address
 LOOP:			# For loop through each pixel
-sw $s4, 0($t9)		# Overwriting the colour at address $t9
+lw $t8, backgroundColour	# Stores the background colour in t8
+sw $t8, 0($t9)		# Overwriting the colour at address $t9
 beq $t9, $s1, END	# Checking if $t9 reached the max
 UPDATE:
 addi $t9, $t9, 4
@@ -116,16 +160,18 @@ jr $ra
 
 # Doodler (x,y)
 DRAW_DOODLER:
-add $t1, $s0, $a2
-add $t2, $t1, $a3
-sw $s6, 0($t2)
+add $t1, $s0, $a2	# Adds the x coordinate to t1
+add $t2, $t1, $a3	# Adds the y coordinate to t2
+lw $t3, doodlerColour	# Adds the doodler colour to t3
+sw $t3, 0($t2)
 jr $ra
 
 # Removes Doodler (x,y)
 REMOVE_DOODLER:
-add $t1, $s0, $a2
-add $t2, $t1, $a3
-sw $s4, 0($t2)
+add $t1, $s0, $a2	# Adds the x coordinate to t1
+add $t2, $t1, $a3	# Adds the y coordinate to t2
+lw $t3, backgroundColour	# adds the background colour to t3
+sw $t3, 0($t2)
 jr $ra
 
 # Platforms
